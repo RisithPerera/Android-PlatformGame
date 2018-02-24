4fa95abc7fe16a523683261d62ae213fcdb296dc
Risith Perera <dgassnerdev@gmail.com>
2018-02-24 23:47:42 +0530

    This is second change

---
+++ app/src/main/java/com/risith/pgame/MainThread.java
@@ -0,0 +1,16 @@
+package com.risith.pgame;
+
+/**
+ * Created by RISITH-PC on 2018-02-24.
+ */
+
+public class MainThread {
+    public MainThread(int c) {
+        System.out.println(c);
+        hello();
+    }
+
+    void hello(){
+        System.out.println("hello");
+    }
+}
